b0367f5053a4ac4c9e1a90ea81c064a68d9f1b09
JannesRuder1 <Jannes.ruder@web.de>
2025-07-18 19:49:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle navbar menu on mobile
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.navbar-toggle');
+    const menu = document.querySelector('.navbar-menu');
+
+    toggleButton.addEventListener('click', function () {
+        menu.classList.toggle('active');
+    });
+});
